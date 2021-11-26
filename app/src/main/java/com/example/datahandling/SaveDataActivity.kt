2f8d45c102b3d9699b4876344c627df7932f1666
Himesh Perera <31988268+HIMESH1018@users.noreply.github.com>
2021-11-26 10:01:38 +0530

    DB Crud Operation Integrated

--- app/src/main/java/com/example/datahandling/SaveDataActivity.kt
+++ app/src/main/java/com/example/datahandling/SaveDataActivity.kt
@@ -1,5 +1,6 @@
 package com.example.datahandling
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +11,7 @@ import com.example.datahandling.DB.DBHelper
 import com.example.datahandling.DB.StudentDetails
 
 class SaveDataActivity : AppCompatActivity() {
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_data)
@@ -25,18 +27,22 @@ class SaveDataActivity : AppCompatActivity() {
         val delete = findViewById<Button>(R.id.delete)
         val dbHelper = DBHelper(this)
 
-        val name = username.text.toString()
-        val id = sid.text.toString()
-        val year = birthyear.text.toString()
-
+        //insert student details
         insert.setOnClickListener(View.OnClickListener {
+            val name = username.text.toString()
+            val id = sid.text.toString()
+            val year = birthyear.text.toString()
 
             if(name.isEmpty()||id.isEmpty()||year.isEmpty()){
                 Toast.makeText(this,"Please Enter Data", Toast.LENGTH_LONG).show()
             }else{
                 if(dbHelper.insertStudent(name,id,year)){
+                    username.setText("")
+                    sid.setText("")
+                    birthyear.setText("")
                     Toast.makeText(this,"Data inserted Successfully",
                         Toast.LENGTH_LONG).show()
+
                 }else{
                     Toast.makeText(this,"Something went Wrong :(",
                         Toast.LENGTH_LONG).show()
@@ -44,8 +50,66 @@ class SaveDataActivity : AppCompatActivity() {
             }
 
         })
-
+        //view data using username
         view.setOnClickListener(View.OnClickListener {
+
+            val name = username.text.toString()
+
+            if (name.isEmpty()){
+                Toast.makeText(this,"Enter the name", Toast.LENGTH_LONG).show()
+            }else {
+                try {
+                    val data = dbHelper.viewStudent(name)
+                    if (data != null && data.moveToFirst()) {
+                        val ids = data.getString(data.getColumnIndex(StudentDetails.Student.COLUMN_NAME_ID))
+                        val birthYears = data.getString(data.getColumnIndex(StudentDetails.Student.COLUMN_NAME_BIRTH_YEAR))
+                        sid.setText(ids)
+                        birthyear.setText(birthYears)
+                    } else {
+                        Toast.makeText(this, "No Data Found", Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: Exception) {
+                    print(e)
+                    Toast.makeText(this, "No Data Found", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+        //delete data using username
+        delete.setOnClickListener(View.OnClickListener {
+
+            val name = username.text.toString()
+            try{
+                if(dbHelper.deleteStudent(name)>0){
+                    Toast.makeText(this,name + " deleted Successfully", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this,name + " not found",Toast.LENGTH_LONG).show()
+                }
+            }catch (e: Exception) {
+                print(e)
+                Toast.makeText(this, "No Data Found", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        //update method
+        update.setOnClickListener(View.OnClickListener {
+
+            val name = username.text.toString()
+            val id = sid.text.toString()
+            val year = birthyear.text.toString()
+            if(name.isEmpty()||id.isEmpty()||year.isEmpty()){
+                Toast.makeText(this,"Please enter data", Toast.LENGTH_LONG).show()
+            }else{
+                try {
+                    if(dbHelper.updateStudent(name,id,year)){
+                        Toast.makeText(this,"Data updated Successfully", Toast.LENGTH_LONG).show()
+                    }
+                }catch (e:Exception){
+                    print(e)
+                    Toast.makeText(this,"Something Went Wrong :(", Toast.LENGTH_LONG).show()
+                }
+            }
+
         })
 
     }
